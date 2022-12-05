28e5b3d46c95ca85f949543e92536643aad6de5a
realitybendinglab <teanshu97@gmail.com>
2022-12-05 18:19:08 +0800

    feedback screen

--- stimuli.js
+++ stimuli.js
@@ -1,20 +1,11 @@
 var stimuli =[
-    {stimulus:'utils/circle.png', label:'You', shape: 'circle'
-    },
-    {stimulus:'utils/circle.png', label:'Friend', shape: 'circle'
-    },
-    {stimulus:'utils/circle.png', label:'Stranger', shape: 'circle'
-    },
-    {stimulus:'utils/triangle.png', label:'You', shape: 'triangle'
-    },
-    {stimulus:'utils/triangle.png', label:'Friend', shape: 'triangle'
-    },
-    {stimulus:'utils/triangle.png', label:'Stranger', shape: 'triangle'
-    },
-    {stimulus:'utils/square.png', label:'You', shape: 'square'
-    },
-    {stimulus:'utils/square.png', label:'Friend', shape: 'square'
-    },
-    {stimulus:'utils/square.png', label:'Stranger', shape: 'square'
-    },
+    {"stimulus":"utils/circle.png", "label":"You", "data": {"screen": "trial", "shape": "circle", "label_cond": "You"}},
+    {"stimulus":"utils/circle.png", "label":"Friend", "data": {"screen": "trial", "shape": "circle", "label_cond": "Friend"}},
+    {"stimulus":"utils/circle.png", "label":"Stranger", "data": {"screen": "trial", "shape": "circle", "label_cond": "Stranger"}},
+    {"stimulus":"utils/triangle.png", "label":"You", "data": {"screen": "trial", "shape": "triangle", "label_cond": "You"}},
+    {"stimulus":"utils/triangle.png", "label":"Friend", "data": {"screen": "trial",  "shape": "triangle", "label_cond": "Friend"}},
+    {"stimulus":"utils/triangle.png", "label":"Stranger", "data": {"screen": "trial", "shape": "triangle", "label_cond": "Stranger"}},
+    {"stimulus":"utils/square.png", "label":"You", "data": {"screen": "trial", "shape": "square", "label_cond": "You"}},
+    {"stimulus":"utils/square.png", "label":"Friend", "data": {"screen": "trial", "shape": "square", "label_cond": "Friend"}},
+    {"stimulus":"utils/square.png", "label":"Stranger", "data": {"screen": "trial", "shape": "square", "label_cond": "Stranger"}},
 ]
