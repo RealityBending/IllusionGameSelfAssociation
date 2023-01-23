55c1145c791522886de626d0d25a395ed575ba2c
realitybendinglab <teanshu97@gmail.com>
2023-01-23 16:42:16 +0800

    fix self task

--- utils/make_shapes.py
+++ utils/make_shapes.py
@@ -31,3 +31,25 @@ draw = PIL.ImageDraw.Draw(image)
 draw.polygon([(20, 220), (230, 220), (125, 20)], fill="red")
 
 image.save(path + "triangle.png")
+
+# Generate Instructions Stim
+## Circle
+circle = ill.image_circles(
+    width=250, height=250, size=1.5, n=1, x=[0], y=[0], color="red", background="white"
+)
+circle.save(path + "demo_circle.png")
+
+## Square
+square = ill.image_rectangle(
+    width=250, height=250, x=0, y=0, size_width=1.5, size_height=1.5, color="red", background="white"
+)
+square.save(path + "demo_square.png")
+
+## Triangle
+
+image = PIL.Image.new("RGBA", (250, 250), color="white")
+# mask = PIL.Image.new("RGBA", (width, height))
+draw = PIL.ImageDraw.Draw(image)
+draw.polygon([(20, 220), (230, 220), (125, 20)], fill="red")
+
+image.save(path + "demo_triangle.png")
