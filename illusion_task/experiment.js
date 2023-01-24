1c9f34be58934c2f41a2db459b80f28ad6303f9e
realitybendinglab <teanshu97@gmail.com>
2023-01-24 10:23:47 +0800

    fix

--- illusion_task/experiment.js
+++ illusion_task/experiment.js
@@ -127,7 +127,7 @@ var make_break2 = {
 }
 
 // Marker
-var marker_position = [0, 0, 200, 200] // [0, 0, 100, 100]
+var marker_position = [0, 0, 0, 0] // [0, 0, 100, 100]
 function create_marker(marker_position, color = "black") {
     const html = `<div id="marker" style="position: absolute; background-color: ${color};\
     left:${marker_position[0]}; top:${marker_position[1]}; \
@@ -403,7 +403,7 @@ const mullerlyer_instructions =
     "<p>Don't get distracted by the surrounding black arrows at the end of the red lines!</p>" +
     "<p>Press <b>the UP or the DOWN arrow</b> to indicate where is the longer <b>red line.</b></p>" +
     "<div style='float: center'><img src='materials/instructions/MullerLyer_Demo.png' height='300'></img>" +
-    "<p><img src='utils/answer/answer_updown_keyboard.PNG' height='150'></img></p>" +
+    "<p><img src='../utils/answer/answer_updown_keyboard.PNG' height='150'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>UP arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
@@ -413,7 +413,7 @@ const ebbinghaus_instructions =
     "<p>Don't get distracted by the surrounding black circles around the red circles!</p>" +
     "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which is the bigger <b>red circle.</b></p>" +
     "<div style='float: center'><img src='materials/instructions/Ebbinghaus_Demo.png' height='300'></img>" +
-    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
+    "<p><img src='../utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
 
@@ -423,6 +423,6 @@ const verticalhorizontal_instructions =
     "<p>Don't get distracted by the orientation of the lines!</p>" +
     "<p>Press <b>the LEFT or the RIGHT arrow</b> to indicate which <b>line is the longer one.</b></p>" +
     "<div style='float: center'><img src='materials/instructions/VerticalHorizontal_Demo.png' height='300'></img>" +
-    "<p><img src='utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
+    "<p><img src='../utils/answer/answer_leftright_keyboard.PNG' height='150'></img></p>" +
     "<p class='small'>In this example, the correct answer is the <b>LEFT arrow</b>.</p></div>" +
     "<p>Are you ready? <b>Press ENTER to start</b></p>"
