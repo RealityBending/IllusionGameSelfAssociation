25ee2afb1f42fd2de2d621619eecd45668c9c70d
Dominique Makowski <dom.mak19@gmail.com>
2024-03-14 10:33:46 +0000

    Update 0_preprocessing.py

--- analysis/0_preprocessing.py
+++ analysis/0_preprocessing.py
@@ -35,14 +35,14 @@ token = ""  # Paste OSF token here to access private repositories
 files = osf_listfiles(
     token=token,
     data_subproject="aw3x9",  # Data subproject ID
-    after_date="10/03/2023",
+    after_date="13/03/2023",
 )
 
 
 # Loop through files ======================================================
 alldata_sub = pd.DataFrame()  # Initialize empty dataframe
 alldata_ig = pd.DataFrame()  # Initialize empty dataframe
-
+alldata_json = pd.DataFrame()  # Initialize empty dataframe
 
 for i, file in enumerate(files):
     print(f"File N°{i+1}/{len(files)}")
@@ -51,3 +51,19 @@ for i, file in enumerate(files):
         if file["name"] in alldata_sub["Participant"].values:
             continue
     data = pd.read_csv(file["file"]._get(file["url"], stream=True).raw)
+
+    if not "browser_info" in data["screen"].values:
+        continue
+    # Browser info -------------------------------------------------------
+    browser = data[data["screen"] == "browser_info"].iloc[0]
+    data["Participant"] = file["name"]
+
+    if "prolific_id" in browser.index:
+        print(" - Prolific")
+    else:
+        continue
+
+    alldata_json = pd.concat([alldata_json, data], axis=0)
+
+
+alldata_json.to_csv("../data/rawdata_json.csv", index=False)
