adcd44e074ecf38111054973ce99273f9ee54dc6
Bapfauvel <bap.fauvel@gmail.com>
2025-02-03 11:35:02 +0100

    Update 0_preprocessing_bis.py
    Change the script to preprocess the data manually downloaded from OSF

--- analysis/0_preprocessing_bis.py
+++ analysis/0_preprocessing_bis.py
@@ -1,41 +1,46 @@
 import json
+import os
 import numpy as np
 import pandas as pd
 
 
-# Get files from OSF ======================================================
-def osf_listfiles(data_subproject="", token="", after_date=None):
-    try:
-        import osfclient
-    except ImportError:
-        raise ImportError("Please install 'osfclient' (`pip install osfclient`)")
-    osf = osfclient.OSF(token=token).project(data_subproject)  # Connect to project
-    storage = [s for s in osf.storages][0]  # Access storage component
-    files = [
-        {
-            "name": file.name.replace(".csv", ""),
-            "date": pd.to_datetime(file.date_created),
-            "url": file._download_url,
-            "size": file.size,
-            "file": file,
-        }
-        for file in storage.files
-    ]
-    file = [f for f in storage.files][0]
-    file.__dict__
+## Get files from OSF ======================================================
+#def osf_listfiles(data_subproject="", token="", after_date=None):
+#    try:
+#        import osfclient
+#    except ImportError:
+#        raise ImportError("Please install 'osfclient' (`pip install osfclient`)")
+#    osf = osfclient.OSF(token=token).project(data_subproject)  # Connect to project
+#    storage = [s for s in osf.storages][0]  # Access storage component
+#    files = [
+#        {
+#            "name": file.name.replace(".csv", ""),
+#            "date": pd.to_datetime(file.date_created),
+#            "url": file._download_url,
+#            "size": file.size,
+#            "file": file,
+#        }
+#        for file in storage.files
+##    ]
+ #   file = [f for f in storage.files][0]
+ #   file.__dict__
+#
+#    if after_date is not None:
+#        date = pd.to_datetime(after_date, format="%d/%m/%Y", utc=True)
+#        files = [f for f, d in zip(files, [f["date"] > date for f in files]) if d]
+#    return files
 
-    if after_date is not None:
-        date = pd.to_datetime(after_date, format="%d/%m/%Y", utc=True)
-        files = [f for f, d in zip(files, [f["date"] > date for f in files]) if d]
-    return files
 
+#token = "zYboMoukFI8HKabenQ35DH6tESHJo6oZll5BvOPma6Dppjqc2jnIB6sPCERCuaqO0UrHAa"  # Paste OSF token here to access private repositories
+#files = osf_listfiles(
+#    token=token,
+#    data_subproject="aw3x9",  # Data subproject ID
+#    after_date="13/03/2023",
+#)
 
-token = "zYboMoukFI8HKabenQ35DH6tESHJo6oZll5BvOPma6Dppjqc2jnIB6sPCERCuaqO0UrHAa"  # Paste OSF token here to access private repositories
-files = osf_listfiles(
-    token=token,
-    data_subproject="aw3x9",  # Data subproject ID
-    after_date="13/03/2023",
-)
+# Path where you have saved and unzipped the data manually downloaded from OSD
+path = "/Users/baptistefauvel/Documents/GitHub/IllusionGameSelfAssociation/osfstorage-archive"
+files = os.listdir(path)
 
 
 # Loop through files ======================================================
@@ -59,6 +64,8 @@ for i, file in enumerate(files):
     if not "browser_info" in data["screen"].values:
         continue
 
+    data = pd.read_csv(path + file)
+    
     # data["screen"].unique()
     # Browser info -------------------------------------------------------
 
